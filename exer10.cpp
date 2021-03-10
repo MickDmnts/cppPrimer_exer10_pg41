4e0d89d0a3190d7172c8250a1aff20ba38f8f683
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-03-10 23:01:13 +0200

    Exercise completed

--- exer10.cpp
+++ exer10.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <string>
 
+void Print2M()
+{
+	std::cout << "2\x4d\n" << std::endl;
+}
+
+void Print2MExtended()
+{
+	std::cout << "2\t\x4d\n" << std::endl; //\t tab, \x4d 'M', \n newline
+}
+
 int main()
 {
+	Print2M();
+	system("pause");
+
+	Print2MExtended();
+
 	return 0;
 }
